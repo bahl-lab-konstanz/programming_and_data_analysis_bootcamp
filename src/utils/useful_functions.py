74e5c03ae4ecce1db82baa4fdf0282a554572c91
Roberto <roberto.garza@mail.polimi.it>
2024-06-12 16:14:37 +0200

    Roberto | clean utils

--- src/utils/useful_functions.py
+++ src/utils/useful_functions.py
@@ -1,6 +1,8 @@
 
 # function to apply to bin edges in output from np.histogram(...) to center them
-#   INPUT: list or array containing bin edges
-#   OUTPUT: list or array with center position of the bins
+#   INPUT
+#       bin_edges: list or array containing bin edges
+#   OUTPUT
+#       bin_centers: list or array with center position of the bins
 def center_bins_hist(bin_edges):
     return (bin_edges[1:] + bin_edges[:-1]) / 2
